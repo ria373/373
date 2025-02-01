ebb2e525e8497aa8ec14f6366c91b0cd8e92567d
ria373 <yutokungenki@icloud.com>
2025-02-01 23:42:50 +0900

    Update form.php

--- form20220713/form20220713/form.php
+++ form20220713/form20220713/form.php
@@ -3,7 +3,7 @@ error_reporting (E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED);
 //
 $mail_sys = "rico.gyomu@icloud.com";	// 管理者のメールアドレス
 $from_name = "お問い合わせメール";	// メール送信者の表示
-$from_mail = "";	// メール送信者のメールアドレス（返信先）
+$from_mail = "rico.gyomu@icloud.com";	// メール送信者のメールアドレス（返信先）
 $user_mail = "item2";	// 利用者にメールを送る場合のメールアドレス項目
 //---
 $title = "お問い合わせフォーム";
